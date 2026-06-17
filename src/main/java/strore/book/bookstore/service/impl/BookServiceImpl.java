52fd08f47b2a79b47b8f2a74334a9b9da133298e
JackWillov <popovuch02@gmail.com>
2026-06-17 04:24:02 +0200

    change constructor to @RequiredArgsConstructor

--- src/main/java/strore/book/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/strore/book/bookstore/service/impl/BookServiceImpl.java
@@ -1,21 +1,17 @@
 package strore.book.bookstore.service.impl;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import strore.book.bookstore.model.Book;
 import strore.book.bookstore.repository.BookRepository;
 import strore.book.bookstore.service.BookService;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
-
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public Book save(Book book) {
